1c3719cc5e91c73f4b842c3f6af24e185b0749b2
TheAnhHe161741 <123942871+TheAnhHe161741@users.noreply.github.com>
2023-03-14 10:44:05 +0700

    Create OrderItems.java

---
+++ Assiginment/src/java/model/OrderItems.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author ADMIN
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItems {
+
+    int id;
+    int orderId;
+    int quantity;
+    int productId;
+    Product product;
+}
