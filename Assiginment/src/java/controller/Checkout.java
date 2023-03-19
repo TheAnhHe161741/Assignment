c8c6eb2e967218c670d3d18406c26de171f1cd98
TheAnhHe161741 <123942871+TheAnhHe161741@users.noreply.github.com>
2023-03-19 23:47:57 +0700

    admin chức năng

---
+++ Assiginment/src/java/controller/Checkout.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.OrderDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Cart;
+import model.User;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Checkout extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Checkout</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Checkout at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        
+        if (session.getAttribute("user") == null) { // Check session.user null thi day ve trang login de login
+            response.sendRedirect("login");
+        }
+        if (session.getAttribute("cart") == null) { // Check session.cart null thi day ve productlist cho nguoi dung add item
+            response.sendRedirect("productList");
+        }
+        User u = (User) session.getAttribute("user"); 
+        Cart c = (Cart) session.getAttribute("cart");
+        // Call DAO
+        OrderDAO odao = new OrderDAO();
+        odao.insertOrder(u, c);
+        // Check out xong thuc hien xoa Cart khoi session
+        session.removeAttribute("cart");
+        // Hien thi script thong bao thanh toan thanh cong --> day nguoi ve trang home
+        response.getWriter().print("<script> alert(\"Order successfully\"); window.location = \"productList\";</script>");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
